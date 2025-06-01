92405b056e5ab71b643f300586d2739a8d82beb2
Livia Mendes <liviimendes2004@gmail.com>
2025-06-01 14:15:22 -0300

    Adiciona arquivos do projeto final de POO

---
+++ Personagem.h
@@ -0,0 +1,45 @@
+#ifndef PERSONAGEM_H
+#define PERSONAGEM_H
+
+#include <iostream>
+using namespace std;
+
+class Personagem {
+    protected:
+        string nome;
+        int vida, ataque, dano;
+    public:
+        // construtor e destrutor 
+        Personagem(string n, int v, int a, int d) : nome{n}, vida{v}, ataque{a}, dano{d} { };
+        virtual ~Personagem() { };
+
+        // métodos de ataque e defesa
+                // ...
+
+        // getters
+        string getNome() const;
+        int getVida() const;
+        int getAtaque() const;
+        int getDefesa() const;
+};
+
+class Jogador : Personagem {
+    protected:
+        int nivel, experiencia;
+        Cenario* cenarioAtual;  // -> composição
+        // vector<Missao*> missoesAtivas;
+        // vector<Habilidade*> habilidadesAprendidas;
+    public:
+        // construtor e destrutor
+        Jogador() : Personagem{"Jogador", 100, 0, 0}, nivel{1}, experiencia{0}, cenarioAtual{nullptr} { };
+        ~Jogador() { };
+
+        // métodos para se mover por cenários
+                // ...
+        // métodos para verificar missões
+                // ...
+
+        void ganharExperiencia(int xp);
+        void subirNivel();
+};
+#endif
