aedbfa09fd859ccb9fa4e0b4104df453e0788f6e
Livia Mendes <liviimendes2004@gmail.com>
2025-06-01 21:19:42 -0300

    Quase término personagem.h

--- Personagem.h
+++ Personagem.h
@@ -13,25 +13,25 @@ class Personagem {
         Personagem(string n, int v, int a, int d) : nome{n}, vida{v}, ataque{a}, dano{d} { };
         virtual ~Personagem() { };
 
-        // métodos de ataque e defesa
+        // métodos de ataque e defesa, virtuais
                 // ...
 
         // getters
-        string getNome() const;
-        int getVida() const;
-        int getAtaque() const;
-        int getDefesa() const;
+        string getNome() const { return nome; }
+        int getVida() const { return vida; }
+        int getAtaque() const { return ataque; }
+        int getDefesa() const { return dano; }
 };
-class Jogador : Personagem {
+class Jogador : public Personagem {
     protected:
-        int nivel, experiencia, testee;
+        int nivel, experiencia;
         Cenario* cenarioAtual;  // -> composição
         // vector<Missao*> missoesAtivas;
         // vector<Habilidade*> habilidadesAprendidas;
     public:
         // construtor e destrutor
-        Jogador() : Personagem{"Jogador", 100, 0, 0}, nivel{1}, experiencia{0}, cenarioAtual{nullptr} { };
-        ~Jogador() { };
+        Jogador(int vida) : Personagem{"Jogador", vida, 10, 5}, nivel{1}, experiencia{0}, cenarioAtual{nullptr} { };
+        virtual ~Jogador() { };
 
         // métodos para se mover por cenários
                 // ...
@@ -41,4 +41,31 @@ class Jogador : Personagem {
         void ganharExperiencia(int xp);
         void subirNivel();
 };
+class Fada : public Jogador {
+    public:
+        // construtor e destrutor
+        Fada() : Jogador{120} { };
+        ~Fada() { };
+
+        // polimorfismo das funções de ataque e defesa
+                // ...
+};
+class Inimigo : public Personagem {
+    public: 
+        // construtor e destrutor
+        Inimigo(int v, int a, int d) : Personagem{"Inimigo", v, a, d} { };
+        ~Inimigo() { };
+
+        // polimorfismo das funções de ataque e defesa
+                // ...
+};
+class MonstroChefe : public Inimigo {
+    protected:
+        string tipo;
+    public: 
+        // construtor e destrutor
+        MonstroChefe(string t = "Monstro Chefe") : Inimigo{100, 50, 5}, tipo{t} { nome = tipo;}
+        ~MonstroChefe() { };
+};
+
 #endif
